ec38465c04113862272268b3377d7a862eec2c6f
Anuradhxx <anuradhajsr2006@gmail.com>
2025-12-27 19:36:16 +0530

    add database file

---
+++ app/database.py
@@ -0,0 +1,29 @@
+# app/database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+URL_DATABASE = os.getenv("URL_DATABASE")
+if not URL_DATABASE:
+    raise ValueError("No database URL found. Please set URL_DATABASE in .env")
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close() 
+
+        
